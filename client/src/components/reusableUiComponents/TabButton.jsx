4058c8ba10daf71ea2ad14987c09cc5944ede5cb
Yana Hanieh <yanahanieh7@gmail.com>
2026-08-29 00:23:40 +0300

    feat: implementing TabButton component in login + fixing port inconsistencies

--- client/src/components/reusableUiComponents/TabButton.jsx
+++ client/src/components/reusableUiComponents/TabButton.jsx
@@ -1,5 +1,5 @@
 function TabButton({label, icon:Icon, isActive, onClick, variant = 'default',disabled = false ,className = ''}){
-    const baseStyles = 'w-full flex items-center gap-2 text-left px-4 py-2.5 rounded-xl font-medium transition-colors cursor-pointer'; //this is the basic style of the tab buttons
+    const baseStyles = 'flex items-center gap-2 text-left px-4 py-2.5 rounded-full font-medium transition-colors cursor-pointer'; //this is the basic style of the tab buttons
 
     const activeStyles = variant === 'danger' ? 'bg-red-500': 'bg-primary text-white'; //style of the danger tab buttons (cancel, delete, no ect) when hovered over/chosen
     const inactiveStyles = variant === 'danger' ? 'text-red-500 hover:bg-red-50' : 'text-neutral-600 hover:text-black'; //basic style of danger tab buttons
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import TextInput from "../components/reusableUiComponents/TextInput";
+import TabButton from "../components/reusableUiComponents/TabButton";
 import { useNavigate } from "react-router-dom";
 
 function LoginPage(){
@@ -11,53 +12,75 @@ function LoginPage(){
 
     const handleSubmit = async (e) =>{
         e.preventDefault();
-      setError('');
-        const targetPath = me.userRole ==='admin' ? '/admin/students' : '/student/courses'
-        navigate(targetPath); 
-  
+        setError('');
+        try{
+            //user Login
+            const res = await fetch('http://localhost:3000/api/auth/login',{
+                method: 'POST', 
+                headers: {'Content-Type': 'application/json'},
+                credentials: 'include', //required for the httpOnly cookie to be set
+                body: JSON.stringify({email,password}), //saves the email and password in the body variable
+            });
 
-        const res = await fetch('http://localhost:300/api/auth/login',{
-            method: 'POST', 
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'include', //required for the httpOnly cookie to be set
-            body: JSON.stringify({email,password}), //saves the email and password in the body variable
-        });
+            //login failed
+            if(!res.ok){
+                setError('Invalid email or password');
+                return;
+            }
 
-        if(!res.ok){ //if the response 
-            setError('Invalid email or password');
-            return;
+            //get the user's info from their current login
+            const meRes = await fetch ('http://localhost:3000/api/auth/me',{ //fetches data from the me breakpoint
+                credentials: 'include',
+            });
+            
+            if(!meRes.ok){
+                setError('Could not retrieve your account Information');
+                return;
+            }
+            
+            const me = await meRes.json(); //saves the fetched data from me endpoint as a me variable
+           
+            if(me.userRole !== role){ //compares if the seleced role is the same as the data fetched from the me breakpoint
+                setError(`This account is registered as ${me.userRole}, not ${role}. Please try again.`);
+                return;
+            }
+
+            //Redirects to the main page based on the user's actual role
+            if(me.userRole === 'admin'){
+                navigate(`/admin/students`);
+            } else if(me.userRole === 'student'){
+                navigate(`/student/courses`);
+            }
+        } catch(error){
+            console.error(error);
+            setError(`Something went wrong. Please try again...`);
         }
-
-        const meRes= await fetch ('http://localhost:300/api/auth/me',{ //fetches data from the me breakpoint
-            credentials: 'include',
-        });
-        const me = await meRes.json(); //saves the fetched data from me endpoint as a me variable
-
-        if(me.userRole !== role){ //compares if the seleced role is the same as the data fetched from the me breakpoint
-            setError(`This account is registered as ${me.userRole}, not ${role}. Please try again.`);
-            return;
-        }
+        // const targetPath = me.userRole ==='admin' ? '/admin/students' : '/student/courses'
+        // navigate(targetPath);            
     };
 
     return(
-        <div className="flex items-center justify-center">
+        <div className="flex min-h-screen items-center justify-center">
             <div className="sm:w-full max-w-sm bg-white shadow-sm rounded-xl border border-neutral-100 p-8">
                 <h1 className="text-center pb-4 text-2xl text-neutral-600 font-semibold">WELCOME BACK</h1>
                 
                 {/* Role toggle */}
-                <div className="relative flex mb-3 p-1 rounded-full bg-neutral-100">
-                    <button 
-                        type ="button"
+                <div className="relative flex mb-3 rounded-full bg-neutral-100">
+
+                    <TabButton
+                        label='Student'
                         onClick={() => setRole('student')}
-                        className={`flex-1 py-2 rounded-full text-md font-medium transition-colors cursor-pointer ${role === 'student'?'bg-primary text-white':'text-neutral-600'}`}>
-                        Student
-                    </button>
-                    <button 
-                        type= "button"
+                        isActive={role === 'student'}
+                        className={`flex-1 py-2 rounded-full text-md font-medium justify-center`}
+                    />
+
+                     <TabButton
+                        label='Admin'
                         onClick={() => setRole('admin')}
-                        className={`flex-1 py-2 rounded-full text-md font-medium  transition-colors cursor-pointer ${role === 'admin'? 'bg-primary text-white':'text-neutral-600'}`}>
-                        Admin
-                    </button>
+                        isActive={role === 'admin'}
+                        className={`flex-1 py-2 rounded-full text-md font-medium justify-center`}
+                    />
+                    
                 </div>
 
                 {/* Data entry */}
@@ -81,7 +104,6 @@ function LoginPage(){
                         />
 
                         {error && <p className="text-sm text-red-500 text-center">{error}</p>}
-
                         <button
                             type='submit'
                             className="py-2 rounded-full bg-neutral-100 hover:bg-secondary text-neutral-600 cursor-pointer">
